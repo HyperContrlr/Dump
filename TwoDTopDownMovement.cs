489f3a668b7cda565c49c2a67d5bc675ce6f9f6a
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2024-09-10 12:33:15 -0700

    some 2d move schtuff

---
+++ TwoDTopDownMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TwoDTopDownMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+    private float activeMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        rb2d.velocity = moveInput * activeMoveSpeed;
+    }
+}
